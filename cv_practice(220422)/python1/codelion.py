78082be899218de5c199f7d158fe1e47e4d34be3
cjs456789@gmail.com <cjs456789@gmail.com>
2022-04-24 19:46:42 +0900

    likelion_HTML/CSS_resume

---
+++ cv_practice(220422)/python1/codelion.py
@@ -0,0 +1,8 @@
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+
+import random
+
+print(random.choice(['된장찌개','피자','제육볶음']))
---
+++ python1/codelion.py
@@ -0,0 +1,6 @@
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+
+print(random.choice(['된장찌개','피자','제육볶음']))
